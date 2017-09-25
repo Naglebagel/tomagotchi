136a9a7faedf65e16ab217fd73bd7364cfd55e73
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-09-25 15:33:47 -0500

    created tomagotchi class

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+console.log('scripts working');
+
+class Tomagotchi {
+	constructor(name){
+		this.name = name,
+		this.hunger = 1,
+		this.boredom = 1,
+		this.sleepiness = 1
+
+	}
+};
+
+const pepe = new Tomagotchi('pepe');
+console.log(pepe);
+
+
