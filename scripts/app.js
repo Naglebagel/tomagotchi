3ddc2c2ded9c7614f4b3e24c7bc77e1d82baead6
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-09-27 09:03:15 -0500

    added background and sprite

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ console.log('scripts working');
 
 class Tomagotchi {
 	constructor(){
-		this.name = name,
+		this.name = '',
 		this.hunger = 1,
 		this.boredom = 1,
 		this.sleepiness = 1,
@@ -15,15 +15,14 @@ class Tomagotchi {
 		this.name = name;
 
 	}
-	chooseSprite(){
-
-	}
 };
 
 const increaseHunger = () => {
 	toma.hunger += 1;
+	$('#tomaHunger').text('Hunger: ' + toma.hunger);
 	if (toma.hunger === 10){
 		alert(toma.name + ' has died please try again')
+		increaseHunger.clearInterval()
 	}
 };
 
@@ -31,8 +30,10 @@ setInterval(increaseHunger, 60*60*1000);
 
 const increaseBoredom = () => {
 	toma.boredom += 1;
+	$('#tomaBoredom').text('Boredom: ' + toma.boredom);
 	if (toma.boredom === 10){
 		alert(toma.name + ' has died please try again')
+		increaseBoredom.clearInterval()
 	}
 };
 
@@ -40,9 +41,10 @@ setInterval(increaseBoredom, 60*60*1000);
 
 const increaseSleepines = () => {
 	toma.sleepiness += 1;
+	$('#tomaSleepiness').text('Sleepiness: ' + toma.sleepiness);
 	if (toma.sleepiness === 10){
 		alert(toma.name + ' has died please try again')
-		clearInterval()
+		increaseSleepiness.clearInterval(toma.sleepiness)
 	}
 };
 
@@ -50,20 +52,68 @@ setInterval(increaseSleepines, 60*60*1000);
 
 const increaseAge = () => {
 	toma.age += 1;
+	$('#tomaAge').text('Age: ' + toma.age);
+	if (toma.age === 10){
+
+	}
 };
 
 setInterval(increaseAge, 60*60*1000);
+
+const decreaseHunger = () => {
+	console.log ('click!!!')
+	if (toma.hunger >= 2){
+		toma.hunger -=1;
+		$('#tomaHunger').text('Hunger: ' + toma.hunger);
+	}
+	else{
+		$('#tomaHunger').text('Hunger: ' + toma.hunger);
+	}
+
+}
+
+const decreaseBoredom = () => {
+		console.log ('click!!!')
+	if (toma.boredom >= 2){
+		toma.boredom -=1;
+		$('#tomaBoredom').text('Boredom: ' + toma.boredom);
+	}
+	else{
+		$('#tomaBoredom').text('Boredom: ' + toma.boredom);
+	}
+}
+
+const decreaseSleepiness = () => {
+		console.log ('click!!!')
+	if (toma.sleepiness >= 2){
+		toma.sleepiness -=1;
+		$('body').toggleClass('toggleSleep');
+		$('#tomaSleepiness').text('Sleepiness: ' + toma.sleepiness);
+	}
+	else{
+		$('#tomaSleepiness').text('Sleepiness: ' + toma.sleepiness);
+	}
+}
 
 const toma = new Tomagotchi();
 console.log(toma);
 toma.setName();
 
-$('#tomaName').text(toma.name);
-$('#tomaAge').text(toma.age);
-$('#tomaHunger').text(toma.hunger);
-$('#tomaBoredom').text(toma.boredom);
-$('#tomaSleepiness').text(toma.sleepiness);
+$('#tomaName').text('Name: ' + toma.name);
+$('#tomaAge').text('Age: ' + toma.age);
+$('#tomaHunger').text('Hunger: ' + toma.hunger);
+$('#tomaBoredom').text('Boredom: ' + toma.boredom);
+$('#tomaSleepiness').text('Sleepiness: ' + toma.sleepiness);
 
+
+$(() => {
+
+  $('#hungerButton').on('click', decreaseHunger);
+  $('#boredomButton').on('click', decreaseBoredom);
+  $('#sleepinessButton').on('click', decreaseSleepiness);
+ 
+
+});
 
 
 
